785219181102e31a8d665df824fa5f48449a900a
shaaravbaruah-bit <shaaravbaruah@gmail.com>
2026-03-13 02:06:25 +0530

    Initial Java port scanner CLI project

---
+++ src/com/network/scanner/PortScanner.java
@@ -0,0 +1,63 @@
+package com.network.scanner;
+
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PortScanner {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== Java Port Scanner =====");
+
+        System.out.print("Enter host: ");
+        String host = sc.nextLine();
+
+        System.out.print("Start port: ");
+        int startPort = sc.nextInt();
+
+        System.out.print("End port: ");
+        int endPort = sc.nextInt();
+
+        System.out.println("\nScanning...\n");
+
+        try {
+
+            FileWriter writer = new FileWriter("scan_result.txt");
+
+            for(int port = startPort; port <= endPort; port++) {
+
+                try {
+
+                    Socket socket = new Socket(host, port);
+                    System.out.println("Port " + port + " OPEN");
+                    writer.write("Port " + port + " OPEN\n");
+                    socket.close();
+
+                }
+
+                catch(Exception e) {
+
+                    System.out.println("Port " + port + " CLOSED");
+                    writer.write("Port " + port + " CLOSED\n");
+
+                }
+            }
+
+            writer.close();
+
+            System.out.println("\nScan finished.");
+            System.out.println("Results saved to scan_result.txt");
+
+        }
+
+        catch(IOException e) {
+            System.out.println("Error writing result file.");
+        }
+
+        sc.close();
+    }
+}
